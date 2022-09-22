2cdffd89e7e613c4410b6336a1b8459845c07e63
Islom <ulmasov_i@yahoo.com>
2022-09-22 22:19:03 +0300

    fix: navigation

--- index.js
+++ index.js
@@ -1,4 +1,69 @@
-function multiply(a, b) {
-  return a * b;
+// function unluckyDays(year){
+//   let date = new Date(year,0).getTime()
+//   console.log(date);
+//   let d = 1000*60*60*24
+//   console.log();
+//   let friday=0
+//   console.time()
+//   for (let i=0;i<365;i++){
+//     date+=d
+//     if (new Date(date).getDay()===5&&new Date(date).getDate()===13) friday++
+//   }
+
+//   return console.log(friday);
+// }
+
+// unluckyDays(2021)
+
+// var name = "Farru===kh";
+// var user = {
+//   name: "Peter",
+//   printMessage() {
+//     console.log(`Hello, ${this.name}!`);
+//   },
+// };
+// var printMessage = user.printMessage;
+// printMessage();
+// function printName() {
+//   console.log(name);
+// }
+// setTimeout(() => {
+//   let name = "Firuz";
+//   printName();
+// }, 1000);
+
+// for (var i = 0; i < 3; i++) {
+//   setTimeout(function () {
+//     console.log(i);
+//   }, 1000);
+// }
+
+// const details = {
+//   message: "Heaaaaaaaaaaaalo",
+// };
+
+// function getMessage() {
+//   return this.message
+// }
+
+// console.log(getMessage.call(details));
+
+// (
+
+// let i = 0;
+// let c = 0;
+// const str = "Rolling Scopes School";
+// while (i < str.length) {
+//   if (str[i] === "o") {
+//     c++;
+//   }
+//   i++;
+// }
+
+for (let i = 0; i <= 4; i++) {
 }
-multiply(2, 2);
+
+console.log(
+  Math.min(10, 222, -5, 0, '-33')
+
+);
